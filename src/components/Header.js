0ed0c9623f0e17c1b5af02dd5f91b32685080884
ADITYA SHARMA <91861881+adityasharma024@users.noreply.github.com>
2024-04-07 14:09:26 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
   const useAppstate=useContext(Appstate);
   return (
     <div className='sticky z-10 header top-0 text-3xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500'>
-      <Link to={'/'}><span>Filmy<span className='text-white'>Verse</span></span></Link>
+      <Link to={'/'}><span>Film<span className='text-white'>Folio</span></span></Link>
       {useAppstate.login?
         <Link to={'/addmovie'}><h1 className='text-lg cursor-pointer flex items-center'>
           <Button><AddIcon className='mr-1' color='secondary' /> <span className='text-white'>Add New</span></Button>
